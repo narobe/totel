2fb569a7cf4e35289eb4083b10db55fb13525cc3
narobe <115933064+narobe@users.noreply.github.com>
2024-08-28 17:01:36 +0300

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,19 @@
+<?php
+$content = '';
+foreach ($_POST as $key => $value) {
+  if($value){
+    $content .="<b>$key</b>: <i>$value</i>\n";
+  }
+  if(trim($content)){
+    $content = "<b>Message from site:</b>\n".$content;
+    # $apiToken = "7202563154:AAFHmlnVmyiZpnEBBSaWhVZpnjZ-c7TliXE";
+    $apiToken = "7256933927:AAFy19j4RNhJ_xsnOypGdafRqWMAY6LNX3Q";
+    $data = [
+      'Chat_id' => '-4523949478',
+      'text' => '$content',
+      'parse_mode' => 'HTML',
+    ];
+    $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data));
+  }
+}
+?>
